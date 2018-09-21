97fae53b3b4bb20f6700400124a7798537e9224b
Unknown <melvin.campos@outlook.be>
2018-09-21 11:04:58 +0200

    August Update
    Small changes, minor bug fixes and personal content added.
    Sorry to push this update quite late; I went to vacation just after finishing this version.

--- js/cv.js
+++ js/cv.js
@@ -18,8 +18,8 @@ $(document).ready(function(){
 $('img').mousedown(function (e) {
   if(e.button == 2) { //right click
     return false; //do nothing
-  });
-}
+  }
+});
 
 /*
  * JS part specific to sidenav
